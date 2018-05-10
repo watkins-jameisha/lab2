fceaa0f2dc4c2492aedbf2f72ddd24279cebd0c0
watkins-jameisha <watkinsjameisha@gmail.com>
2018-05-10 16:54:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let friends =['matt','jacey','shawn','cole','keke'
+];
+for(let i=0;i<friends.length;i++){
+    for(let j=99;j > 0;j--){
+        if(j>2) {
+            console.log(j + "lines of code in the file ," + j + "lines of code ;" + friends + "strikes one out," + "clears it all out" + (j-1) + "lines of code in the file")
+        }else if(j===2) { 
+            console.log(j + "line of code in the file," + j + "line of code;" + friends + "strikes one out," + "clears it all out," + (j-1) + " line of code in the file")
+        } else {
+            console.log(j + "line of code in the file," + j + "line of code;" + friends + "strikes one out," + "clears it all out,","no more lines of code in the file")
+        }
+    }
+}
